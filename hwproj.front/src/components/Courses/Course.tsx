72194ff102f2ead58b5d8454d38db16e33323e8a
Alex Berezhnykh <alexey.berezhnykh@jetbrains.com>
2023-04-21 21:42:32 +0300

    Fix "edit course" link

--- hwproj.front/src/components/Courses/Course.tsx
+++ hwproj.front/src/components/Courses/Course.tsx
@@ -208,7 +208,7 @@ const Course: React.FC<RouteComponentProps<ICourseProps>> = (props) => {
                                         </IconButton>
                                     }
                                     {isMentor && !isReadingMode! && (
-                                        <RouterLink to={"./edit"}>
+                                        <RouterLink to={`/courses/${courseId}/edit`}>
                                             <EditIcon fontSize="small"/>
                                         </RouterLink>
                                     )}
