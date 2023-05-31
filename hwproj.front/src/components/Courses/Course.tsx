946eafdbf5a8dde798f68d3af05d30d328f96bd6
YuriUfimtsev <ufimtsev3007@gmail.com>
2023-05-31 14:32:21 +0300

    fix: fix the occurred errors

--- hwproj.front/src/components/Courses/Course.tsx
+++ hwproj.front/src/components/Courses/Course.tsx
@@ -84,7 +84,7 @@ const Course: React.FC<RouteComponentProps<ICourseProps>> = (props) => {
     })
 
     const [pageState, setPageState] = useState<IPageState>({
-        tabValue: "homeworks"
+        tabValue: isFromYandex ? "stats" : "homeworks"
     })
 
     const {
